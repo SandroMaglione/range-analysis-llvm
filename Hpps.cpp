39709a36eaba59e762362c49355ef5befba1a751
Sandro Maglione <lass.maglio@gmail.com>
2020-05-19 18:32:34 +0200

    Added support for sub opcode instructions

--- Hpps.cpp
+++ Hpps.cpp
@@ -24,8 +24,8 @@ namespace
     bool runOnFunction(Function &Func) override
     {
       // Keep reference of value still left to find the ranges
-      // 'from' is unknown, it depends on 'to' and 'toValue'
-      // from = to + toValue
+      // 'from' is unknown, it depends on 'to', 'toValue', and 'toOps'
+      // from = to toOps('+' or '-') toValue
       std::vector<Value *> from;
       std::vector<Value *> to;
       std::vector<int> toValue;
@@ -55,12 +55,13 @@ namespace
           // 'toValue' -> Operand1 int value
           if (auto *operInst = dyn_cast<BinaryOperator>(&I))
           {
-            Value *oper0 = operInst->getOperand(0);
+            // Value *oper0 = operInst->getOperand(0);
             Value *oper1 = operInst->getOperand(1);
 
+            // Store opcode ('+' or '-') to find real value range
             if (operInst->getOpcode() == Instruction::Add)
             {
-              errs() << "   -Stored:" << operInst->getName() << "\n";
+              errs() << "   -Stored(Add):" << operInst->getName() << "\n";
 
               // Store reference of variable still to find range
               from.push_back(operInst);
@@ -76,6 +77,19 @@ namespace
             }
             else if (operInst->getOpcode() == Instruction::Sub)
             {
+              errs() << "   -Stored(Sub):" << operInst->getName() << "\n";
+
+              // Store reference of variable still to find range
+              from.push_back(operInst);
+              to.push_back(loadRef.at(0));
+              toOps.push_back(operInst->getOpcode());
+              if (ConstantInt *CI = dyn_cast<ConstantInt>(oper1))
+              {
+                toValue.push_back(CI->getZExtValue());
+              }
+
+              // Remove previous used load instruction value
+              loadRef.pop_back();
             }
           }
 
@@ -126,27 +140,36 @@ namespace
 
       // Print references
       errs() << "\nREFERENCES:\n";
-      for (auto i = 0; i < from.size(); ++i)
+      for (unsigned i = 0; i < from.size(); ++i)
       {
         errs() << to.at(i)->getName() << "+" << toValue.at(i) << "<-" << from.at(i)->getName() << "\n";
 
         // Search variable reference inside already found ranges
-        for (int v = 0; v < ranged.size(); ++v)
+        for (unsigned v = 0; v < ranged.size(); ++v)
         {
           // When range found, add a new found range
           if (to.at(i) == ranged.at(v))
           {
             errs() << "-FOUND:" << ranged.at(v)->getName() << "\n";
-            minRange.push_back(minRange.at(v) + toValue.at(i));
-            maxRange.push_back(minRange.at(v) + toValue.at(i));
+            if (toOps.at(i) == Instruction::Add)
+            {
+              minRange.push_back(minRange.at(v) + toValue.at(i));
+              maxRange.push_back(minRange.at(v) + toValue.at(i));
+            }
+            else if (toOps.at(i) == Instruction::Sub)
+            {
+              minRange.push_back(minRange.at(v) - toValue.at(i));
+              maxRange.push_back(minRange.at(v) - toValue.at(i));
+            }
+
             ranged.push_back(from.at(i));
           }
         }
       }
 
       // Print value range computed
-      errs() << "\nVALUE RANGES:\n";
-      for (auto i = 0; i < minRange.size(); ++i)
+      errs() << "\nVALUE RANGES\n";
+      for (unsigned i = 0; i < minRange.size(); ++i)
       {
         errs() << ranged.at(i)->getName() << "(" << minRange.at(i) << ", " << maxRange.at(i) << ")\n";
       }
