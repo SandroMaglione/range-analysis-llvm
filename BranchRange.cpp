cb7c1ad0fd83d3f59c5b47c98a65319e45ccd47f
Sandro Maglione <lass.maglio@gmail.com>
2020-05-25 19:20:58 +0200

    Added helper functions

--- BranchRange.cpp
+++ BranchRange.cpp
@@ -74,7 +74,33 @@ namespace
 
                     if (auto *operInst = dyn_cast<BinaryOperator>(I))
                     {
-                        errs() << "Oper" << operInst << "\n";
+                        // Get operands from binary operation
+                        Value *oper0 = operInst->getOperand(0);
+                        Value *oper1 = operInst->getOperand(1);
+
+                        // Print variable assigned and name of operands
+                        errs() << "   -Var: " << operInst->getName() << "\n";
+                        errs() << "     -Op0: " << oper0->getName() << "\n";
+                        errs() << "     -Op1: " << oper1->getName() << "\n";
+
+                        // // a = b + 1 (variable + constant)
+                        // if (ConstantInt *CI = dyn_cast<ConstantInt>(oper1))
+                        // {
+                        //     toValue.push_back(CI->getZExtValue());
+                        //     to2.push_back(nullValue);
+
+                        //     // If operand0 is reference (and not an alias) store it
+                        //     if (oper0->hasName())
+                        //     {
+                        //         to.push_back(oper0);
+                        //     }
+                        //     // Otherwise get value from previous load instruction (alias)
+                        //     else
+                        //     {
+                        //         to.push_back(loadRef.at(0));
+                        //         loadRef.pop_back();
+                        //     }
+                        // }
                     }
                     else if (auto *brInst = dyn_cast<BranchInst>(I))
                     {
@@ -137,7 +163,7 @@ namespace
                             // TODO: First sweep ranges are fine.
                             // If you now see on ReMarkable notes, when there is the br back to while.cond
                             // we need to compute %add, since we found a new range for it (so we need to check BinaryOperations).
-                            // We must then add the range to the while.cond BB and add it again to the 
+                            // We must then add the range to the while.cond BB and add it again to the
                             // workList with this new information
 
                             // Check for new range and add it in case of updates
@@ -227,7 +253,7 @@ namespace
                 {
                     // Already in the list but range has changed, COMBINE the ranges
                     errs() << "ALREADY THERE!\n";
-                    std::pair<int, int> newPairRange(std::min(valRange.first, rangeSuccessor.first), std::max(valRange.second, rangeSuccessor.second));
+                    std::pair<int, int> newPairRange = combine(valRange, rangeSuccessor);
                     listRange->find(BB)->second.erase(listRange->find(BB)->second.begin() + i);
                     listRange->find(BB)->second.push_back(std::pair<Value *, std::pair<int, int>>(oper, newPairRange));
                     return true;
@@ -237,14 +263,57 @@ namespace
             return false;
         }
 
+        // Combine two ranges to compute their new value
+        std::pair<int, int> combine(std::pair<int, int> range0, std::pair<int, int> range1)
+        {
+            return std::pair<int, int>(std::min(range0.first, range1.first), std::max(range0.second, range1.second));
+        }
+
+        // Given vector of references and new range, checks if value is already present and range values are changed
+        bool isVariableNotPresentOrChanged(Value *oper, std::vector<std::pair<Value *, std::pair<int, int>>> *listRangeBB, std::pair<int, int> rangeSuccessor)
+        {
+            if (listRangeBB->size() == 0)
+            {
+                // Not present
+                return true;
+            }
+
+            // Check if it is already in the list and range changed, if not, return false (i.e. no update)
+            for (unsigned i = 0; i < listRangeBB->size(); ++i)
+            {
+                Value *val = listRangeBB->at(i).first;
+                std::pair<int, int> valRange = listRangeBB->at(i).second;
+
+                if (val == oper && (valRange.first != rangeSuccessor.first || valRange.second != rangeSuccessor.second))
+                {
+                    // Already present and changed
+                    return true;
+                }
+                if (val == oper && valRange.first == rangeSuccessor.first && valRange.second == rangeSuccessor.second)
+                {
+                    // Already present and not changed
+                    return false;
+                }
+            }
+
+            // Not present
+            return true;
+        }
+
+        // Check if given BasicBlock is already inside the workList
+        bool isInWorkList(BasicBlock *next, std::vector<BasicBlock *> *workList)
+        {
+            return std::find(workList->begin(), workList->end(), next) != workList->end();
+        }
+
         // If isToAdd is true, then range changed, therefore add to list in any case
         void updateWorkList(bool isToAdd, BasicBlock *next, std::map<BasicBlock *, std::vector<std::pair<Value *, std::pair<int, int>>>> *listRange, std::vector<BasicBlock *> *workList)
         {
             // Check successor1 if already visited
             std::map<BasicBlock *, std::vector<std::pair<Value *, std::pair<int, int>>>>::iterator it = listRange->find(next);
 
-            // If not already visited, add to workList
-            if (isToAdd || it == listRange->end())
+            // If not already visited, add to workList (do not add if already present)
+            if ((isToAdd || it == listRange->end()) && !isInWorkList(next, workList))
             {
                 workList->push_back(next);
                 errs() << "New BB in workList\n";
